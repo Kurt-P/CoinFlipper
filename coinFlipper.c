111ed30ba31167fb767f4480053178c214058021
Kurt P <krprudhomme@gmail.com>
2015-06-11 12:15:50 -0400

    Update coinFlipper.c
    Added logic and other stuff

--- coinFlipper.c
+++ coinFlipper.c
@@ -1,6 +1,31 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 int main(int argc, char *argv[]) {
-    
+	
+	int x, randomNumber, flips;
+	unsigned int seed;
+	
+	if (argc < 3) {
+		printf("Usage: %s <seed>, <flips>", argv[0]);
+		return -1;
+	}
+	
+	seed = atoi(argv[1]);
+	flips = atoi(argv[2]);
+	
+	srand(seed);
+	
+	for (x = 0; x <= flips; x++) {
+		randomNumber = rand();
+		
+		if (randomNumer % 2 == 0) {
+			printf("Heads");
+		}
+		else {
+			printf("Tails");
+		}
+	}
+	
     return 0;
 }
